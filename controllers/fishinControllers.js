4bb62df2b29e6435b5e5d7ed6ba5ffc16a69b22b
brody <49502879+meloknight@users.noreply.github.com>
2024-05-02 13:39:33 -0700

    moved more code into controllers file. cleaned up index.js for readability.

---
+++ controllers/fishinControllers.js
@@ -0,0 +1,114 @@
+const pool = require("../db");
+
+const getAllFish = async (req, res) => {
+  try {
+    console.log(req.requestTime);
+    const allFish = await pool.query("SELECT * FROM fish");
+    res.json(allFish.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const createFish = async (req, res) => {
+  try {
+    const {
+      fish_name,
+      fish_mean_weight_kg,
+      fish_weight_boundary_kg,
+      fish_mean_length_cm,
+      fish_length_boundary_cm,
+      rarity,
+    } = req.body;
+    const newFish = await pool.query(
+      "INSERT INTO fish (fish_name, fish_mean_weight_kg, fish_weight_boundary_kg, fish_mean_length_cm, fish_length_boundary_cm, rarity) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
+      [
+        fish_name,
+        fish_mean_weight_kg,
+        fish_weight_boundary_kg,
+        fish_mean_length_cm,
+        fish_length_boundary_cm,
+        rarity,
+      ]
+    );
+    res.status(200).json(newFish.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const getSpecificFish = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const fish = await pool.query("SELECT * FROM fish WHERE fish_id = $1", [
+      id,
+    ]);
+    if (!fish.rows[0]) {
+      res.status(400).send(`Fish with id ${id} is not in database`);
+    }
+    res.json(fish.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const goFishin = async (req, res) => {
+  try {
+    // fishListLength = 12;
+    const fishListLength = await pool.query(
+      "SELECT COUNT(fish_name) FROM fish;"
+    );
+    const random = Math.random();
+    // console.log(random);
+    const randomNumber = Math.ceil(
+      random * Number(fishListLength.rows[0].count)
+    );
+    // console.log(randomNumber);
+    const fish = await pool.query("SELECT * FROM fish WHERE fish_id = $1", [
+      randomNumber,
+    ]);
+
+    console.log(fish.rows[0]);
+
+    res.status(200).json(fish.rows[0]);
+    // res.status(200).json(fishListLength.rows[0].count);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const updateFish = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { fish_name } = req.body;
+    console.log(`The fish_id is: ${id} and the fish_name will be ${fish_name}`);
+    const updateFish = await pool.query(
+      "UPDATE fish SET fish_name = $1 WHERE fish_id = $2",
+      [fish_name, id]
+    );
+    res.status(200).json({ completed: true });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+const deleteFish = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteFish = await pool.query("DELETE FROM fish WHERE fish_id = $1", [
+      id,
+    ]);
+    res.json({ completed: true });
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+module.exports = {
+  getAllFish,
+  createFish,
+  getSpecificFish,
+  goFishin,
+  updateFish,
+  deleteFish,
+};
--- index.js
+++ index.js
@@ -1,128 +1,36 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
 const myLogger = require("./middleware/logger");
 const requestTime = require("./middleware/requestTime");
+const {
+  getAllFish,
+  createFish,
+  getSpecificFish,
+  goFishin,
+  updateFish,
+  deleteFish,
+} = require("./controllers/fishinControllers");
 
-require("dotenv").config();
-
-// middleware
+// middleware //
 app.use(cors());
 app.use(express.json()); //req.body and req.params
 app.use(myLogger);
 app.use(requestTime);
 
-// ROUTES //
+// routes //
+app.route("/api/v1/fish").post(createFish).get(getAllFish);
+app
+  .route("/api/v1/fish/searchfish/:id")
+  .get(getSpecificFish)
+  .put(updateFish)
+  .delete(deleteFish);
+app.get("/api/v1/fish/fishin", goFishin);
 
-// create a fish
-app.post("/api/v1/fish", async (req, res) => {
-  try {
-    const {
-      fish_name,
-      fish_mean_weight_kg,
-      fish_weight_boundary_kg,
-      fish_mean_length_cm,
-      fish_length_boundary_cm,
-      rarity,
-    } = req.body;
-    const newFish = await pool.query(
-      "INSERT INTO fish (fish_name, fish_mean_weight_kg, fish_weight_boundary_kg, fish_mean_length_cm, fish_length_boundary_cm, rarity) VALUES($1, $2, $3, $4, $5, $6) RETURNING *",
-      [
-        fish_name,
-        fish_mean_weight_kg,
-        fish_weight_boundary_kg,
-        fish_mean_length_cm,
-        fish_length_boundary_cm,
-        rarity,
-      ]
-    );
-
-    res.status(200).json(newFish.rows[0]);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-// get all fish
-app.get("/api/v1/fish", async (req, res) => {
-  try {
-    console.log(req.requestTime);
-    const allFish = await pool.query("SELECT * FROM fish");
-    res.json(allFish.rows);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-// get a particular fish
-app.get("/api/v1/fish/searchfish/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const fish = await pool.query("SELECT * FROM fish WHERE fish_id = $1", [
-      id,
-    ]);
-    res.json(fish.rows[0]);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-// go fishin! Random fish for now. Expand out to only fish in selected region, at certain time, etc.
-app.get("/api/v1/fish/fishin", async (req, res) => {
-  try {
-    // fishListLength = 12;
-    const fishListLength = await pool.query(
-      "SELECT COUNT(fish_name) FROM fish;"
-    );
-    const random = Math.random();
-    // console.log(random);
-    const randomNumber = Math.ceil(
-      random * Number(fishListLength.rows[0].count)
-    );
-    // console.log(randomNumber);
-    const fish = await pool.query("SELECT * FROM fish WHERE fish_id = $1", [
-      randomNumber,
-    ]);
-    res.status(200).json(fish.rows[0]);
-    // res.status(200).json(fishListLength.rows[0].count);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-// update a fish
-app.put("/api/v1/fish/searchfish/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { fish_name } = req.body;
-    console.log(`The fish_id is: ${id} and the fish_name will be ${fish_name}`);
-    const updateFish = await pool.query(
-      "UPDATE fish SET fish_name = $1 WHERE fish_id = $2",
-      [fish_name, id]
-    );
-    res.status(200).json({ completed: true });
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-// delete a fish
-app.delete("/api/v1/fish/searchfish/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deleteFish = await pool.query(
-      "DELETE FROM fishlist WHERE fish_id = $1",
-      [id]
-    );
-    res.json({ completed: true });
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
+// listener //
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => {
   console.log(`server has started on port ${port}`);
 });
