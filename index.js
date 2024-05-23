259e75728fc9c9fceb0a889bd00dcb327efd6cff
brody <49502879+meloknight@users.noreply.github.com>
2024-05-23 14:21:47 -0700

    Remove a package.json test script that appears to escape the github action. Saved a copy of the script at bottom of index.js.

--- index.js
+++ index.js
@@ -45,3 +45,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server has started on port ${port}`);
 });
+
+// "test": "echo \"Error: no test specified\" && exit 1"
